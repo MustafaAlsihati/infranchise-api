8c36ae5a18c4eee524f75a379127176a924e8790
MustafaAlsihati <itwix-x@hotmail.com>
2023-08-31 14:24:10 +0300

    Cards routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -54,12 +54,20 @@ router.get('/cards/business/:id', async (req, res) => {
       throw new Error('Business not found');
     }
 
+    business['First Name'] = business['First Name'].trim();
+    business['Last Name'] = business['Last Name'].trim();
+    business['Company'] = business['Company'].trim();
+    business['Job Title'] = business['Job Title'].trim();
+    business['Email'] = business['Email'].trim();
+
+    const username = business['First Name'] + ' ' + business['Last Name'];
+
     let html = BusinessMeetingTemplate;
-    // html = html.replace('{{TITLE}}', '');
-    // html = html.replace('{{NAME}}', '');
-    // html = html.replace('{{COMPANY_NAME}}', '');
-    // html = html.replace('{{POSITION}}', '');
-    // html = html.replace('{{EMAIL}}', '');
+    html = html.replace('{{TITLE}}', username);
+    html = html.replace('{{NAME}}', username);
+    html = html.replace('{{COMPANY_NAME}}', business['Company']);
+    html = html.replace('{{POSITION}}', business['Job Title']);
+    html = html.replace('{{EMAIL}}', business['Email']);
 
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
@@ -81,12 +89,20 @@ router.get('/cards/exhibitor/:id', async (req, res) => {
       throw new Error('Exhibitor not found');
     }
 
+    exhibitor['First Name'] = exhibitor['First Name'].trim();
+    exhibitor['Last Name'] = exhibitor['Last Name'].trim();
+    exhibitor['Company Name'] = exhibitor['Company Name'].trim();
+    exhibitor['Brand Name'] = exhibitor['Brand Name'].trim();
+    exhibitor['Email'] = exhibitor['Email'].trim();
+
+    const username = exhibitor['First Name'] + ' ' + exhibitor['Last Name'];
+
     let html = ExhibitorTemplate;
-    // html = html.replace('{{TITLE}}', '');
-    // html = html.replace('{{NAME}}', '');
-    // html = html.replace('{{COMPANY_NAME}}', '');
-    // html = html.replace('{{POSITION}}', '');
-    // html = html.replace('{{EMAIL}}', '');
+    html = html.replace('{{TITLE}}', username);
+    html = html.replace('{{NAME}}', username);
+    html = html.replace('{{COMPANY_NAME}}', exhibitor['Company Name']);
+    html = html.replace('{{POSITION}}', exhibitor['Brand Name']);
+    html = html.replace('{{EMAIL}}', exhibitor['Email']);
 
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
@@ -106,12 +122,17 @@ router.get('/cards/visitor/:id', async (req, res) => {
       throw new Error('Visitor not found');
     }
 
+    visitor['Name'] = visitor['Name'].trim();
+    visitor['Company Name'] = visitor['Company Name'].trim();
+    visitor['Occupation'] = visitor['Occupation'].trim();
+    visitor['Email'] = visitor['Email'].trim();
+
     let html = VisitorTemplate;
-    // html = html.replace('{{TITLE}}', '');
-    // html = html.replace('{{NAME}}', '');
-    // html = html.replace('{{COMPANY_NAME}}', '');
-    // html = html.replace('{{POSITION}}', '');
-    // html = html.replace('{{EMAIL}}', '');
+    html = html.replace('{{TITLE}}', visitor['Name']);
+    html = html.replace('{{NAME}}', visitor['Name']);
+    html = html.replace('{{COMPANY_NAME}}', visitor['Company Name']);
+    html = html.replace('{{POSITION}}', '');
+    html = html.replace('{{EMAIL}}', visitor['Email']);
 
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
